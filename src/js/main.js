822087fd9621825ec5cf0edd5cdc8e7ee0c4dddc
qrbriggs-byui <briggsq@byui.edu>
2025-05-01 12:05:00 -0600

    Updates

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,11 @@ const parkData = getParkData();
 const disclaimer = document.querySelector(".disclaimer > a");
 disclaimer.href = parkData.url;
 disclaimer.innerHTML = parkData.fullName;
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="hero-banner__title">${info.name}</a>
+    <p class="hero-banner__subtitle">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>`;
+  }
--- src/public/examples/week2/js/alice.js
+++ src/public/examples/week2/js/alice.js
@@ -7,14 +7,14 @@ function parseAlice(mystring){
 document.getElementById("go-alice").addEventListener('click', ()=>{
     parseAlice(document.getElementById("og-alice").value)
     alert("Check the console to see the alice object.")
-    console.log(parsedObject.name);        // Output: Alice
-    console.log(parsedObject.age);         // Output: 30
-    console.log(parsedObject.courses[0]);  // Output: Math
-    console.log(parsedObject.address.city); // Output: Rexburg    
+    console.log(alice.name);        // Output: Alice
+    console.log(alice.age);         // Output: 30
+    console.log(alice.courses[0]);  // Output: Math
+    console.log(alice.address.city); // Output: Rexburg    
 })
 
 document.getElementById("stringify-alice").addEventListener('click', ()=>{
-    alice.awesomenessIndex = document.getElementById("awesomeness").value
+    alice.awesomenessIndex = parseInt(document.getElementById("awesomeness").value)
     document.getElementById("new-alice").value = JSON.stringify(alice)
-    document.getElementById("new-alice").value = JSON.stringify(alice, null, 2)
+    // document.getElementById("new-alice").value = JSON.stringify(alice, null, 2)
 })
